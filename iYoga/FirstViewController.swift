a1e82e3d116572ac05e2f7bb6caccc4d157a4a26
Kaustubh <kaustubh.pnj@gmail.com>
2020-06-22 23:32:25 +0530

    Initial Commit of Project

---
+++ iYoga/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  iYoga
+//
+//  Created by Kaustubh on 22/06/20.
+//  Copyright © 2020 Kaustubh. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iYoga/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  iYoga
+//
+//  Created by Kaustubh on 22/06/20.
+//  Copyright © 2020 Kaustubh. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
